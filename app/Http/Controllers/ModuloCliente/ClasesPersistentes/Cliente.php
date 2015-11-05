cdefe63782c3de06f51f6ed0ba24dff9a6c1f13f
JhonVargasT <jhonvargast@gmail.com>
2015-11-04 22:58:56 -0500

    commit 1.1

---
+++ app/Http/Controllers/ModuloCliente/ClasesPersistentes/Cliente.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: JhonVargasN72V
+ * Date: 4/11/2015
+ * Time: 10:55 PM
+ */
+class Cliente extends Persona
+{
+
+
+}
---
+++ app/Http/Controllers/ModuloCliente/ClasesPersistentes/Persona.php
@@ -0,0 +1,229 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: JhonVargasN72V
+ * Date: 4/11/2015
+ * Time: 10:45 PM
+ */
+class Persona extends controll
+{
+    private $nombre;
+    private $apellido;
+    private $sexo;
+    private $docuIdent;
+    private $fechaNacimiento;
+    private $email;
+    private $ciudad;
+    private $direccion;
+    private $referenciasLocali;
+    private $telefonoFijo;
+    private $telefonoMobil;
+
+    /**
+     * Persona constructor.
+     * @param $nombre Nombre de la persona
+     * @param $apellido Apellido de la persona
+     * @param $sexo Sexo de la persona
+     * @param $docuIdent Documento de identidad de la personas Dni o Ruc
+     * @param $fechaNacimiento Fecha de nacimiento de la persona
+     * @param $email Correo electronico
+     * @param $ciudad Ciudad de residencia
+     * @param $direccion direccion de rresidencia
+     * @param $referenciasLocali Referencias de como ubicar ala persona
+     * @param $telefonoFijo Telefono
+     * @param $telefonoMobil celular
+     */
+    public function __construct($nombre, $apellido, $sexo, $docuIdent, $fechaNacimiento, $email, $ciudad, $direccion, $referenciasLocali, $telefonoFijo, $telefonoMobil)
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->sexo = $sexo;
+        $this->docuIdent = $docuIdent;
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->email = $email;
+        $this->ciudad = $ciudad;
+        $this->direccion = $direccion;
+        $this->referenciasLocali = $referenciasLocali;
+        $this->telefonoFijo = $telefonoFijo;
+        $this->telefonoMobil = $telefonoMobil;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * @param mixed $apellido
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * @param mixed $sexo
+     */
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDocuIdent()
+    {
+        return $this->docuIdent;
+    }
+
+    /**
+     * @param mixed $docuIdent
+     */
+    public function setDocuIdent($docuIdent)
+    {
+        $this->docuIdent = $docuIdent;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    /**
+     * @param mixed $fechaNacimiento
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+
+    /**
+     * @param mixed $ciudad
+     */
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @param mixed $direccion
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReferenciasLocali()
+    {
+        return $this->referenciasLocali;
+    }
+
+    /**
+     * @param mixed $referenciasLocali
+     */
+    public function setReferenciasLocali($referenciasLocali)
+    {
+        $this->referenciasLocali = $referenciasLocali;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefonoFijo()
+    {
+        return $this->telefonoFijo;
+    }
+
+    /**
+     * @param mixed $telefonoFijo
+     */
+    public function setTelefonoFijo($telefonoFijo)
+    {
+        $this->telefonoFijo = $telefonoFijo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefonoMobil()
+    {
+        return $this->telefonoMobil;
+    }
+
+    /**
+     * @param mixed $telefonoMobil
+     */
+    public function setTelefonoMobil($telefonoMobil)
+    {
+        $this->telefonoMobil = $telefonoMobil;
+    }
+
+
+}

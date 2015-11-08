25e5cd249811cdfc1c3225fa1e7940a12fdac99e
JhonVargasT <jhonvargast@gmail.com>
2015-11-08 13:24:57 -0500

    commit 1.5

---
+++ app/Http/Controllers/pruebaBD.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JhonVargasN72V
+ * Date: 8/11/2015
+ * Time: 1:24 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class pruebaBD
+{
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('num1/{num1}/num2/{num2}','probar@suma');
